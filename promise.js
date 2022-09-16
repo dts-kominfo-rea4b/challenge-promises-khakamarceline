e94cd6825f66445a8097d5aa51dbd8983fe383dc
khakamarceline <84651701+khakamarceline@users.noreply.github.com>
2022-09-16 21:56:36 +0700

    revisi promise.js

--- promise.js
+++ promise.js
@@ -2,15 +2,21 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 
-const promiseOutput(emosi)
-  .then((marah) => {
-    console.log(marah);
-  })
-  .catch((tidakMarah) => {
-    console.log("err", tidakMarah);
-  });
+const emosi = true;
 
+const promiseOutput = new Promise((resolve, reject) => {
+  if (emosi) {
+    const membuatMarah = "Marah";
+    resolve(membuatMarah);
+  } else {
+    const tidakMarah = "tidak marah";
+    reject(tidakMarah);
+  }
+});
 
+promiseOutput.then((ok) => console.log(ok)).catch((fail) => console.log(fail));
+
+  
 module.exports = {
   promiseOutput,
 };
